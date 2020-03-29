6fd5ea0c0872d6d231b1218b1e7192d5473a2e03
Nicholas Drone <ndrone@gmail.com>
2020-03-29 18:24:12 -0400

    Created RG and VNET

--- main.tf
+++ main.tf
@@ -3,8 +3,21 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
+
+variable "web_server_location" {}
+variable "web_server_rg" {}
+variable "resource_prefix" {}
+variable "web_server_address_space" {}
+
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "eastus2"
+resource "azurerm_resource_group" "web_server_rg" {
+  name     = var.web_server_rg
+  location = var.web_server_location
+}
+
+resource "azurerm_virtual_network" "web_server_vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+  address_space       = [var.web_server_address_space]
 }
