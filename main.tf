37e20fba4d82269f4816c7d8afc2bbc91e4a7928
Nicholas Drone <ndrone@gmail.com>
2020-03-29 19:06:18 -0400

    Created subnet and nic

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ variable "web_server_location" {}
 variable "web_server_rg" {}
 variable "resource_prefix" {}
 variable "web_server_address_space" {}
+variable "web_server_address_prefix" {}
+variable "web_server_name" {}
 
 # Create a resource group
 resource "azurerm_resource_group" "web_server_rg" {
@@ -20,4 +22,28 @@ resource "azurerm_virtual_network" "web_server_vnet" {
   location            = var.web_server_location
   resource_group_name = azurerm_resource_group.web_server_rg.name
   address_space       = [var.web_server_address_space]
+  # The below subnet doesn't work with a nic
+  # subnet {
+  #   name           = "${var.resource_prefix}-subnet"
+  #   address_prefix = var.web_server_address_prefix
+  # }
+}
+
+resource "azurerm_subnet" "web_server_subnet" {
+  name                 = "${var.resource_prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.web_server_rg.name
+  virtual_network_name = azurerm_virtual_network.web_server_vnet.name
+  address_prefix       = var.web_server_address_prefix
+}
+
+resource "azurerm_network_interface" "web_server_nic" {
+  name                = "${var.web_server_name}-nic"
+  location            = var.web_server_location
+  resource_group_name = var.web_server_rg
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web_server_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
