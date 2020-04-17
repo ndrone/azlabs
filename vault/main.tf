c8fc705882cd118ab43963a564897bb2f995423c
Nicholas Drone <ndrone@gmail.com>
2020-04-16 21:58:35 -0400

    Created Key Vault

---
+++ vault/main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.2.0"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+variable "vault_location" {}
+variable "vault_rg" {}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "vault_rg" {
+  name     = var.vault_rg
+  location = var.vault_location
+}
+
+resource "azurerm_key_vault" "vault_kv" {
+  name                        = "${var.vault_rg}-kv2"
+  location                    = azurerm_resource_group.vault_rg.location
+  resource_group_name         = azurerm_resource_group.vault_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete",
+      "recover",
+      "backup",
+      "restore"
+    ]
+  }
+}
---
+++ vault/terraform.tfvars
@@ -0,0 +1,2 @@
+vault_location      = "eastus2"
+vault_rg            = "vault-rg"
