9ea057ed3c7db17c13942126601ed8f46e2b018e
Nicholas Drone <ndrone@gmail.com>
2020-05-03 17:11:57 -0400

    Added Sping Boot project for pipelines deploy

---
+++ pipelines/java/azpipelines/src/main/java/org/example/azpipelines/AzPipelinesApplication.java
@@ -0,0 +1,14 @@
+package org.example.azpipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzPipelinesApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzPipelinesApplication.class, args);
+	}
+
+}
