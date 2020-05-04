c36037f37fafb784bc544c6fa47ad5f9c0b0a5a2
Nicholas Drone <ndrone@gmail.com>
2020-05-03 21:47:42 -0400

    Added actuator since the rest controller wasn't working???

--- pipelines/java/azpipelines/src/test/java/org/example/azpipelines/AzPipelinesApplicationTests.java
+++ pipelines/java/azpipelines/src/test/java/org/example/azpipelines/AzPipelinesApplicationTests.java
@@ -1,21 +1,14 @@
 package org.example.azpipelines;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class AzPipelinesApplicationTests
 {
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Test
     void contextLoads()
     {
-        Assertions.assertThat(applicationContext.getBean(HelloController.class)).isNotNull();
     }
 
 }
